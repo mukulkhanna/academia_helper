65d14c314a722a983e276da7838e63984b2344a6
Mukul Khanna <mukulkhanna@Mukuls-MacBook-Air.local>
2017-10-08 03:05:12 +0530

    Dynamically opening course feedback page

--- academia_srm_feedback.py
+++ academia_srm_feedback.py
@@ -16,5 +16,11 @@ browser.find_element_by_xpath("//input[@id='Email']").send_keys("mukulkhanna_ra@
 browser.find_element_by_xpath("//input[@id='Password']").send_keys("dafuqsrm")
 browser.find_element_by_xpath("""//*[@id="signinForm"]/div[5]/input""").click()
 
+#logged in till this point ^
+#browser.implicitly_wait(15)
+
+WebDriverWait(browser,25).until(
+    EC.presence_of_element_located((By.XPATH,"""//*[@id="zc-container"]/tbody/tr[1]/td/div[5]/table/tbody/tr/td[6]""")))
+browser.find_element_by_xpath("""//*[@id="zc-container"]/tbody/tr[1]/td/div[5]/table/tbody/tr/td[6]""").click()
 
 
