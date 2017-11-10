e7708154194d1745eec6a4699332d7bef9c9605b
Mukul Khanna <mukul18khanna@gmail.com>
2017-11-11 02:31:25 +0530

    working now

--- academia_srm_feedback.py
+++ academia_srm_feedback.py
@@ -4,34 +4,50 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 browser=webdriver.Chrome('../../../Documents/chromedriver')
 browser.get('http://academia.srmuniv.ac.in')
-frame = browser.find_element_by_xpath("//iframe[@urltype='zc_cpLoginUrl']")
+frame = browser.find_element_by_xpath("""//*[@id="signinPage"]/div[2]/iframe""")
 browser.switch_to_frame(frame)
-WebDriverWait(browser, 3).until(
-    EC.frame_to_be_available_and_switch_to_it((By.NAME, "zohoiam"))
-    )
-browser.find_element_by_xpath("//input[@id='Email']").send_keys("mukulkhanna_ra@srmuniv.edu.in")
-browser.find_element_by_xpath("//input[@id='Password']").send_keys("academiawalapassword")
+WebDriverWait(browser,25).until(
+    EC.presence_of_element_located((By.XPATH,"""//input[@id='Email']""")))
+browser.find_element_by_xpath("//input[@id='Email']").send_keys("ENTERACADEMIAUSERNAMEHERE")
+browser.find_element_by_xpath("//input[@id='Password']").send_keys("ENTERACADEMIAPASSWORDHERE")
 browser.find_element_by_xpath("""//*[@id="signinForm"]/div[5]/input""").click()
 
 #logged in till this point ^
 
-
 WebDriverWait(browser,25).until(
-    EC.presence_of_element_located((By.XPATH,"""//*[@id="zc-container"]/tbody/tr[1]/td/div[5]/table/tbody/tr/td[6]""")))
-browser.find_element_by_xpath("""//*[@id="zc-container"]/tbody/tr[1]/td/div[5]/table/tbody/tr/td[6]""").click()
-
-#opened feedback page ^
-
-WebDriverWait(browser,25).until(
-    EC.presence_of_element_located((By.XPATH,"""//*[@id="Student_Feedback_Form_2016_17_3"]/div[1]/form/div/table/tbody/tr/td/table/tbody/tr[6]/td/div/div[2]/table/tbody/tr[4]""")))  #waiting for row-element to load
+    EC.presence_of_element_located((By.XPATH,"""//*[@id="zc-container"]/tbody/tr[1]/td/div[5]/table/tbody/tr/td[7]""")))  #waiting for row-element to load
 
 #waiting for table rows to load ^
 
-select_span=browser.find_element_by_xpath("""//*[@id="Student_Feedback_Form_2016_17_3"]/div[1]/form/div/table/tbody/tr/td/table/tbody/tr[6]/td/div/div[2]/table/tbody/tr[4]/td[4]/div/div[1]/li/span""")
+select_span=browser.find_element_by_xpath("""//*[@id="zc-container"]/tbody/tr[1]/td/div[5]/table/tbody/tr/td[7]""")
 
-select_span.click()         #expands the dropdown
+select_span.click()
+time.sleep(10)       
+actions = ActionChains(browser) 
+actions.send_keys(Keys.TAB * 6)
+for j in range(0,7):
+    for i in range(0,14):
+        actions.send_keys(Keys.DOWN * 1)
+        actions.send_keys(Keys.ENTER * 1)
+    actions.send_keys(Keys.TAB * 3)
+               
+actions.send_keys(Keys.TAB * 2)
+
+for j in range(0,4):
+    for i in range(0,14):
+        actions.send_keys(Keys.DOWN * 1)
+        actions.send_keys(Keys.ENTER * 1)
+    
+    actions.send_keys(Keys.TAB * 3)
+    
+actions.perform()
+
+time.sleep(2)
+browser.find_element_by_xpath("""//*[@id="Student_Feedback_Form_2016_17_3"]/div[1]/form/table/tbody/tr/td/span/input[1]""").click()
