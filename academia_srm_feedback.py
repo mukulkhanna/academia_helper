60720548b38578e26a76b290a28d46d285a5884a
Mukul Khanna <mukul18khanna@gmail.com>
2017-11-23 12:44:44 +0530

    Random remark selection added

--- academia_srm_feedback.py
+++ academia_srm_feedback.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
-
+import random
 
 browser=webdriver.Chrome('../../../Documents/chromedriver')
 browser.get('http://academia.srmuniv.ac.in')
@@ -15,8 +15,8 @@ frame = browser.find_element_by_xpath("""//*[@id="signinPage"]/div[2]/iframe""")
 browser.switch_to_frame(frame)
 WebDriverWait(browser,25).until(
     EC.presence_of_element_located((By.XPATH,"""//input[@id='Email']""")))
-browser.find_element_by_xpath("//input[@id='Email']").send_keys("ENTERACADEMIAUSERNAMEHERE")
-browser.find_element_by_xpath("//input[@id='Password']").send_keys("ENTERACADEMIAPASSWORDHERE")
+browser.find_element_by_xpath("//input[@id='Email']").send_keys("")
+browser.find_element_by_xpath("//input[@id='Password']").send_keys("")
 browser.find_element_by_xpath("""//*[@id="signinForm"]/div[5]/input""").click()
 
 #logged in till this point ^
@@ -34,7 +34,7 @@ actions = ActionChains(browser)
 actions.send_keys(Keys.TAB * 6)
 for j in range(0,7):
     for i in range(0,14):
-        actions.send_keys(Keys.DOWN * 1)
+        actions.send_keys(Keys.DOWN * random.randint(1, 5))
         actions.send_keys(Keys.ENTER * 1)
     actions.send_keys(Keys.TAB * 3)
                
@@ -42,7 +42,7 @@ actions.send_keys(Keys.TAB * 2)
 
 for j in range(0,4):
     for i in range(0,14):
-        actions.send_keys(Keys.DOWN * 1)
+        actions.send_keys(Keys.DOWN * random.randint(1, 5))
         actions.send_keys(Keys.ENTER * 1)
     
     actions.send_keys(Keys.TAB * 3)
